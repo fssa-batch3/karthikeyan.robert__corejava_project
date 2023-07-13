58343aeb2ad58051bbacacc0cb80f721b711301f
karthi354 <116268464+karthi354@users.noreply.github.com>
2023-07-13 19:52:26 +0530

    day04 completed

---
+++ src/main/java/day04/Misc/Square.java
@@ -0,0 +1,7 @@
+package day04.Misc;
+
+class Square extends Rectangle {
+	public Square(double side) {
+		super(side, side);
+	}
+}
---
+++ src/main/java/day04/practice/RightAngledTriangle.java
@@ -0,0 +1,34 @@
+package day04.practice;
+public class RightAngledTriangle extends Polygon {
+	protected double a;
+	protected double b;
+	protected double c;
+
+	public RightAngledTriangle(double a, double b, double c) throws Exception {
+		super(4);
+		if (a <= 0 || b <= 0 || c <= 0) {
+			throw new Exception("Invalid dimensions for a rectangle");
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double calculateArea() {
+		return (this.a * this.b) / 2;
+	}
+
+	public double calculatePerimeter() {
+		return (this.a + this.b + this.c);
+	}
+
+	public int getNumberOfSides() {
+		return super.numberOfSides;
+	}
+
+	public double circumference() {
+
+		return 0;
+	}
+
+}
